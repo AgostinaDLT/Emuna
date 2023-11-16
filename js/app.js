123b49776ce2d8e3c2d05f5f1aee71e95a81d115
AgostinaDLT <ag.os.tinadelatorre@hotmail.com>
2023-11-16 19:16:52 -0300

    actualizacion9

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+const shopContent = document.getElementById("shopContent");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+
 const productos=[
     {
         id: 1,
@@ -80,3 +84,31 @@ let carrito = [];
 // }else if(seleccion == "no"){
 //     alert("Gracias por pasar por nuestra pagina");
 // }
+
+productos.forEach((product)=>{
+    let content = document.createElement("div");
+    content.className = "card"
+    content.innerHTML = `
+    <img src="${product.img}">
+    <h3>${product.nombre}</h3>
+    <p class="seccion-texto">${product.precio} $</p>
+    `;
+    shopContent.append(content);
+
+    let comprar = document.createElement("button");
+    comprar.innerText = "Comprar";
+    comprar.className = "Comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", ()=>{
+        carrito.push({
+            id: product.id,
+            img: product.img,
+            nombre: product.nombre,
+            precio: product.precio}
+        );
+        console.log(carrito);
+    })
+})
+
--- js/carrito.js
+++ js/carrito.js
@@ -1,26 +1,62 @@
-//Variable que mantiene el estado visible del carrito
-var carritoVisible = false;
+const pintarCarrito = () =>{
+    modalContainer.innerHTML = "";
+    modalContainer.style.display = "flex";
+    const modalHeader = document.createElement("div");
+    modalHeader.className = "modal-header";
+    modalHeader.innerHTML =`
+    <h1 class="modal-header-title">Carrito</h1>
+    `;
 
-//Espera de carga de productos
-if(document.readyState=='loading'){
-  document.addEventListener('DOMContentLoaded', ready)
-}else{
-  ready();
-}
+    modalContainer.append(modalHeader);
 
+    const modalButton = document.createElement("h1");
+    modalButton.innerText="X";
+    modalButton.className = "modal-header-button";
 
-function ready(){
-  //Funcionalidad de los botones de eliminar
-  var botonesElimiarItem = document.getElementsByClassName('btn-eliminar');
-  for(vari=0; i<botonesElimiarItem.length;i++){
-    var button =  botonesElimiarItem[i];
-    button.addEventListener('click', eliminarItemCarrito);
-  }
-}
+    modalButton.addEventListener("click", ()=>{
+        modalContainer.style.display = "none";
+    });
 
+    modalHeader.append(modalButton);
 
-//eliminar el item seleccionado del carrito
-function eliminarItemCarrito(event){
-  var buttonClicked = event.target;
-  
-}
+    carrito.forEach((product)=>{
+      let carritoContent = document.createElement("div");
+     carritoContent.className = "modal-content";
+     carritoContent.innerHTML = `
+     <img src="${product.img}">
+     <h3>${product.nombre}</h3>
+     <p>${product.precio} $</p>   
+       `  ;
+
+       modalContainer.append(carritoContent);
+
+       let eliminar = document.createElement("span");
+
+       eliminar.innerText = "X";
+       eliminar.className = "delete-product";
+       carritoContent.append(eliminar);
+
+       eliminar.addEventListener("click", eliminarProducto);
+    });
+
+    const total = carrito.reduce((acc, el) => acc + el.precio, 0);
+
+    const totalBuying = document.createElement("div")
+    totalBuying.className = "total-content"
+    totalBuying.innerHTML = `total a pagar: ${total} $`;
+    modalContainer.append(totalBuying);
+
+    
+};
+
+verCarrito.addEventListener("click", pintarCarrito);
+
+const eliminarProducto = () =>{
+    const foundId = carrito.find((element) => element.id);
+    
+    carrito = carrito.filter((carritoId) =>{
+        return carritoId !==  foundId;
+    });
+
+    pintarCarrito();
+};
